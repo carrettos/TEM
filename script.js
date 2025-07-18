87aff7d4828eee38b80676236ffebfc5bd3ecd36
carrettos <h.carretto97@gmail.com>
2025-07-17 22:52:00 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("service-worker.js")
-    .then(() => console.log("Service Worker registrado ✅"))
-    .catch(error => console.log("Error en SW:", error));
-}
 const datos = {
   hipovolemico: {
     titulo: "💧 Shock Hipovolémico",
